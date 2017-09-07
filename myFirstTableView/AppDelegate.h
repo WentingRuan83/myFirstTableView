85619368dffbc40bf9a6bb9018d08c4f43623f9b
Wenting Ruan <wentingruan@WentingdeMacBook-Air.local>
2017-09-06 18:27:56 -0700

    Initial Commit

---
+++ myFirstTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myFirstTableView
+//
+//  Created by Wenting Ruan on 2017/9/6.
+//  Copyright © 2017年 Wenting Ruan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myFirstTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myFirstTableView
+//
+//  Created by Wenting Ruan on 2017/9/6.
+//  Copyright © 2017年 Wenting Ruan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
